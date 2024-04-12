1b19392bd48358822708bd551265f0d923300ed2
Navtoj Chahal <github@navtoj.com>
2024-04-12 09:02:52 -0700

    Only Update Latest Term

--- timetables/run.ts
+++ timetables/run.ts
@@ -12,42 +12,40 @@ await ensureDir('public/timetables');
 // get available terms
 const terms = await getTerms();
 
-// // get latest term
-// const latestTerm = terms.sort((a, b) => a.code.localeCompare(b.code)).at(-1);
-// // make sure a term is available
-// if (!latestTerm) {
-// 	console.log('No terms available.');
-// 	Deno.exit(1);
-// }
-
-for (const term of terms) {
-	// SCRAPE DATA
-
-	const scraped = await scrape(term);
-
-	// FORMAT DATA
-
-	const formatted = scraped; // format(scraped);
-
-	// VALIDATE DATA
-
-	const validated = Timetable.safeParse(formatted);
-	if (!validated.success) {
-		core.error(`Timetable (${term.code}) failed validation.`);
-		console.log(validated.error.issues);
-		Deno.exit(1);
-	}
-
-	// SAVE DATA
-
-	await Deno.writeTextFile(
-		`public/timetables/${term.code}.json`,
-		JSON.stringify(
-			validated.data,
-			null,
-			'\t',
-		),
-	);
+// get latest term
+const term = terms.sort((a, b) => a.code.localeCompare(b.code)).at(-1);
+// make sure a term is available
+if (!term) {
+	console.log('No terms available.');
+	Deno.exit(1);
 }
+
+// SCRAPE DATA
+
+const scraped = await scrape(term);
+
+// FORMAT DATA
+
+const formatted = scraped; // format(scraped);
+
+// VALIDATE DATA
+
+const validated = Timetable.safeParse(formatted);
+if (!validated.success) {
+	core.error(`Timetable (${term.code}) failed validation.`);
+	console.log(validated.error.issues);
+	Deno.exit(1);
+}
+
+// SAVE DATA
+
+await Deno.writeTextFile(
+	`public/timetables/${term.code}.json`,
+	JSON.stringify(
+		validated.data,
+		null,
+		'\t',
+	),
+);
 
 console.timeEnd('timetables');
